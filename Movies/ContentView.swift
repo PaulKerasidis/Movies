c5a2b3682f0125ac1868b8f5805f53f563e4aba5
Pkerasidis <p.kerasidis@icloud.com>
2025-01-19 17:31:10 +0200

    Initial Commit

---
+++ Movies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movies
+//
+//  Created by Paulos Kerasidis on 19/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Paulos Kerasidis on 19/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
